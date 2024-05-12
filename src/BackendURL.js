f388c1a3eeaef0fcbd6f4d7a1919d1d9ed6aa347
Yasirwan <afereviewer5@gmail.com>
2024-05-12 21:50:34 +0700

    back

--- src/BackendURL.js
+++ src/BackendURL.js
@@ -1,4 +1,4 @@
-const BackendURL = "https://belajarin-sr-production.up.railway.app"
+const BackendURL = "http://localhost:4500"
 // const BackendURL = "http://47.236.0.158/"
 // const BackendURL = "http://localhost:4500"
 
