e3053f859a059ff7ee28663513c429df9fb0eeea
Tsalsabilla <tsalsabilla569@gmail.com>
2024-05-13 16:34:06 +0700

    /

--- src/BackendURL.js
+++ src/BackendURL.js
@@ -1,4 +1,4 @@
-const BackendURL = "https://belajar-in-server.vercel.app"
+const BackendURL = "https://belajar-in-server.vercel.app/"
 // const BackendURL = "https://belajarin-sr-production.up.railway.app"
 // >>>>>>> 034e5f61ea654c63dc110f66a82723fc0edfde3d
 // const BackendURL = "http://47.236.0.158/"
