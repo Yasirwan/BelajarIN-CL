f3cbd48f5e75cf3c5c39a7e0f7906f70c0c21c3d
Tsalsabilla <tsalsabilla569@gmail.com>
2024-05-19 21:28:52 +0700

    form

--- src/Pages/SingleDoubt/SingleDoubt3.jsx
+++ src/Pages/SingleDoubt/SingleDoubt3.jsx
@@ -111,18 +111,18 @@ const SingleDoubt3 = () => {
           </div>
         </div>
 
-        <div className="assignmentResponses bg-customBlue rounded-lg p-4 my-2">
-          <h3 className="text-white font-bold text-center">Form Pengumpulan</h3>
-        </div>
-        
+
         <div className="assignmentResponses bg-customBlue rounded-lg p-4 my-2">
           <h3 className="text-white font-bold text-center">Lembar Kerja Peserta Didik (LKPD)</h3>
         </div>
 
-
         <div className="grid justify-items-center">
           <p className="p-4">Silahkan mengakses LKPD </p>
           <a target="_blank" href="https://docs.google.com/document/d/1jY2kj_ex-tgT1GmVWfqNV_NAkLC6yGvk/edit?usp=sharing&ouid=112525555696106018803&rtpof=true&sd=true" class="inline-block bg-cyan-400 text-white font-bold py-2 px-4 rounded-md">View</a>
+        </div>
+
+        <div className="assignmentResponses bg-customBlue rounded-lg p-4 my-2">
+          <h3 className="text-white font-bold text-center">Form Pengumpulan</h3>
         </div>
 
         <div className="doubt3Responses bg-white rounded-lg p-4 my-2">
